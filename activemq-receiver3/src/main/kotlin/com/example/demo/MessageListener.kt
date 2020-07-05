aab104742976857d43325e0d2b4c74366bf6c66e
veluxer <valuexer62@gmail.com>
2020-07-05 16:09:24 +0900

    Write virtual topic model

--- activemq-receiver3/src/main/kotlin/com/example/demo/MessageListener.kt
+++ activemq-receiver3/src/main/kotlin/com/example/demo/MessageListener.kt
@@ -9,28 +9,12 @@ import java.io.Serializable
 class MessageListener {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-    @JmsListener(destination = "VirtualTopic.TEST")
-    fun listen3(message: SampleData) {
-        logger.info("Server3 Virtual1 - > $message")
-    }
-
-    @JmsListener(destination = "VirtualTopic.TEST")
-    fun listen4(message: SampleData) {
-        logger.info("Server3 Virtual2 - > $message")
-    }
-
-//    @JmsListener(destination = "Consumer.test3.VirtualTopic.TEST")
-//    fun listen5(message: SampleData) {
-//        logger.info("Server2 Virtual1 - > $message")
-//    }
-
-    @JmsListener(destination = "Consumer.test3.VirtualTopic.TEST")
-    fun listen6(message: SampleData) {
-//        val err = 1 / 0
-        logger.info("Server2 Virtual2 - > $message")
+    @JmsListener(destination = "TOPIC.ORDER")
+    fun listen(message: OrderMessage) {
+        logger.info("Consumer Listen - > $message")
     }
 }
 
-data class SampleData(
-        val body: String = ""
+data class OrderMessage(
+        val payload: String = ""
 ) : Serializable
